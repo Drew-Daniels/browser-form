aa6afc0e88a5106be41980461cdf9cd461f79a0d
Drew Daniels <drew@thedaniels.com>
2022-01-13 13:55:04 -0600

    Finished creating custom validation.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,5 @@
 'use strict';
 
-// const unhide = function(element, visibleClass) {
-//   element.classList.add(visibleClass);
-// }
-// const hide = function(element, visibleClass) {
-//   element.classList.remove(visibleClass);
-// }
-// const getClasses = function(element) {
-//   const classList = element.className.split(' ');
-//   return classList;
-// }
 const classify = function(element, ...classesToAdd) {
   element = element.classList.add(...classesToAdd);
   return element;
@@ -28,7 +18,8 @@ const userCountry = document.querySelector('#user-country');
 const userZIP = document.querySelector('#user-zip');
 const userPwd = document.querySelector('#user-pwd');
 const userPwdCnf = document.querySelector('#user-pwd-cnf');
-class Control {
+
+class BaseControl {
   constructor(HTMLElement, validationMsg) {
     this.HTMLElement = HTMLElement;
     this.validationMsg = validationMsg;
@@ -41,38 +32,75 @@ class Control {
   }
 }
 
-const email = new Control(userEmail, 'Please enter a valid email address');
-const country = new Control(userCountry, 'Please enter a valid country');
-const ZIP = new Control(userZIP, 'Please enter a valid ZIP code');
-const pwd = new Control(userPwd, 'Please enter a password');
-const pwdCnf = new Control(userPwdCnf, 'Password does not match')
+class TypeControl extends BaseControl {
+  constructor(HTMLElement, validationMsg) {
+    super(HTMLElement, validationMsg);
+  }
+  fails() {
+    return this.HTMLElement.validity.typeMismatch;
+  }
+}
+
+class PatternControl extends BaseControl {
+  constructor(HTMLElement, validationMsg) {
+    super(HTMLElement, validationMsg);
+  }
+  fails() {
+    return (
+      this.HTMLElement.validity.patternMismatch ||
+      this.HTMLElement.validity.tooShort ||
+      this.HTMLElement.validity.tooLong
+    )
+      ;
+  }
+}
+
+class PwdCnfControl extends BaseControl {
+  constructor(HTMLElement, validationMsg, siblingControl) {
+    super(HTMLElement, validationMsg);
+    this.siblingControl = siblingControl;
+  }
+  fails() {
+    if (this.siblingControl.HTMLElement.value !== this.HTMLElement.value) {
+      return true;
+    }
+    else return false;
+  }
+}
+
+const emailControl = new TypeControl(userEmail, 'Please enter a valid email address');
+const countryControl = new TypeControl(userCountry, 'Please enter a valid country');
+
+const ZIPControl = new PatternControl(userZIP, 'Please enter a valid 5-digit ZIP code');
+const pwdControl = new PatternControl(userPwd, 'Please enter a password with a minimum of 8 characters, at least 1 letter, 1 number, and 1 special character');
+const pwdCnfControl = new PwdCnfControl(userPwdCnf, 'Password does not match', pwdControl);
 
 const controls = [
-  email,
-  country,
-  ZIP,
-  pwd,
-  pwdCnf,
+  emailControl,
+  countryControl,
+  ZIPControl,
+  pwdControl,
+  pwdCnfControl,
 ]
 
 function addListeners(formControls) {
   formControls.forEach(formControl => {
     let formElement = formControl.getHTMLElement();
-    formElement.addEventListener('input', function(event) {
+    formElement.addEventListener('input', function() {
       let errIcon = formElement.nextElementSibling;
       let sucIcon = errIcon.nextElementSibling;
-      if (formElement.validity.typeMismatch) {
+      if (formControl.fails()) {
         let msg = formControl.getValidationMsg();
         formElement.setCustomValidity(msg);
         declassify(sucIcon, 'visible');
         classify(errIcon, 'visible');
-        // formElement.reportValidity();
+        formElement.reportValidity();
       } else {
         formElement.setCustomValidity('');
         declassify(errIcon, 'visible');
         classify(sucIcon, 'visible');
       }
-    })
+    });
   })
 }
 
